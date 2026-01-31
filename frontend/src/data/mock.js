61be13f6df00951640b0daa6853c0457a657d61e
Md Asif <yb.fisa@gmail.com>
2026-01-31 14:34:22 +0530

    ...

--- frontend/src/data/mock.js
+++ frontend/src/data/mock.js
@@ -13,7 +13,7 @@ export const siteConfig = {
     showOpenSource: true,
     showAbout: true,
     showFooter: true,
-    showTestimonials: true,
+    showTestimonials: false,
   },
 
   // Header/Navigation
